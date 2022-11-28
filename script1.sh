735ba472f789827f69a83f4ced76af2101ab0662
olajumoke olusile <jummai02@yahoo.com>
2022-11-28 04:15:18 +0000

    script1.sh

---
+++ script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "welcome to landmark technologies"
+echo "you and yours are now a millionaires"
+echo "congratulations, you graduated with jobs"
+mkdir billions
+touch billions/millionaires
+echo "Bello" >> billions/millionaires
