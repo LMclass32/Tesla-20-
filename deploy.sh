284d0b952303088f40367bd79bd6052c10f8ee80
jumoke2022 <118619273+jumoke2022@users.noreply.github.com>
2022-12-05 21:35:34 -0700

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "deployment in progress"
-sudo mkdir deploygroup1 
-sudo touch deploygroup1/app.java
-echo "welcome to Tesla" >> deploygroup1/app.java
-sudo chmod 777 deploygroup1/app.java
-sudo chown dominio deploygroup1/app.java
-
